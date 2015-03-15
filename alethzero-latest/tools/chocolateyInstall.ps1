5044304b0ca5906cc97578306d7981adda9000df
Wilfried Kopp <chevdor@gmail.com>
2015-03-15 16:44:30 +0100

    Switch to 7zip.commandline
    ref #2

--- alethzero-latest/tools/chocolateyInstall.ps1
+++ alethzero-latest/tools/chocolateyInstall.ps1
@@ -10,7 +10,7 @@ Invoke-WebRequest $source -OutFile "$destination"
 # if (-not (test-path "$env:ProgramFiles\7-Zip\7z.exe")) {throw "$env:ProgramFiles\7-Zip\7z.exe needed"} 
 # set-alias sz "$env:ProgramFiles\7-Zip\7z.exe"  
 
-7z x -aoa -y -o"$env:ProgramFiles\$packageName\" "$destination"
+7za x -aoa -y -o"$env:ProgramFiles\$packageName\" "$destination"
 
 Write-Host Cleaning up the downloaded file from the temp folder.
 remove-item "$destination"
